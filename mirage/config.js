005f03ee50e44aa47b83c1356989f9c3ab4f2ff5
vikalejo <vikalejo@gmail.com>
2017-05-18 00:05:03 +0200

    adapter to use mirage api

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,35 @@
 export default function() {
+  this.namespace = '/api';
+
+  this.get('/items', function(){
+    return {
+      data:[{
+        type: 'items',
+        id: 'Recycling',
+        attributes: {
+          title: 'Glass CD sky',
+          owner: 'Maria A',
+          city: 'Valladolid',
+          type: 'Paint',
+          price: 8,
+          image: 'https://upload.wikimedia.org/wikipedia/en/1/1a/Microwaved-CD-2.jpg',
+          description: 'Old CD painted by me.'
+          }
+        }, {
+        type: 'items',
+        id: 'Painting',
+        attributes: {
+          title: 'Moonlight',
+          owner: 'Rosi M',
+          city: 'Valladolid',
+          type: 'Paint',
+          price: 100,
+          image: 'https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FarMu4f8rnBk%2Fmaxresdefault.jpg&f=1',
+          description: 'My monlight version'
+        }
+      }]
+    };
+  });
 
   // These comments are here to help you get started. Feel free to delete them.
 
